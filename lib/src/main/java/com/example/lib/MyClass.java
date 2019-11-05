04fa0407563e83b763e4b2ba2fa9a876aeb60aa3
liushufan1104 <aeeoprsd110414@gmail.com>
2019-11-05 19:05:09 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+
+public class MyClass{
+    public static void main(String [] args){
+            System.out.println("birgthday is?");
+           java.util.Scanner scanner = new java.util.Scanner(System.in);
+            int num1 = scanner.nextInt();
+            System.out.println("birthday is\n" + num1);
+
+    }
+}
+
